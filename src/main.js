770e7236a04d0f82a741614a5dbdfbe6e952a22b
echerevko <43856956+echerevko@users.noreply.github.com>
2021-03-06 22:07:23 +0100

    Changed the default search city

--- src/main.js
+++ src/main.js
@@ -139,4 +139,4 @@ fahrenheitLink.addEventListener('click', displayFahrenheitTemperature);
 let celsiusLink = document.querySelector('#celsius-link');
 celsiusLink.addEventListener('click', displayCelsiusTemperature);
 
-search('kiev');
+search('stockholm');
